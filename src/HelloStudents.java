ebc8eeff5014f450e5ca706709860fd26c13f36a
ANA KHASANAH <ana.khasanah@capita.com>
2022-11-18 17:53:44 +0000

    Added print commands to create square picture

--- src/HelloStudents.java
+++ src/HelloStudents.java
@@ -1,5 +1,11 @@
 public class HelloStudents {
     public static void main(String[] args) {
         System.out.println("Hello, this is your first program in Java");
+
+        System.out.println("-----------");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("------------");
     }
 }
